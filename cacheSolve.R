64d56592755b3e939fc1d53d04a93a570d6bb267
gharango <gustavo.arango@oracle.com>
2015-03-20 22:32:04 +0100

    Create cacheSolve.R
    This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
    I assume that the matrix supplied is always squared invertible.

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+  inv <- x$getsolve()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setsolve(inv)
+  inv
+}
